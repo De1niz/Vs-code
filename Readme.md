c8e49becd480f2bbb228fe06d5b92412590ec2a2
De1niz <erdogde4957_aehit@students.vizja.pl>
2024-10-15 00:27:03 +0200

    first commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+:wave: Hello, I'm Deniz Erdogan 
+
+In this area you will see my projects that I have prepared in Visual Studio Code.
+
+I hope you like it 
---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main () {
+    int a = 5 ;
+    int b = 3 ;
+    double c = 2.2;
+    int  answer ;
+
+std :: cout << "enter a number : ";
+
+std :: cin >> answer;
+
+if (answer ==10 ){
+
+std::cout << "YES" << std::endl;
+
+} else {
+
+std::cout << "NO"  <<  std::endl;
+
+}
+
+return 0 ; 
+
+}
